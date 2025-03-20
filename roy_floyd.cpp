5aef43c12b70c3197b1931c8f7c091c6cca95d7a
Hmarius06 <158442908+Hmarius06@users.noreply.github.com>
2025-03-20 10:35:56 +0200

    Create roy_floyd.cpp

---
+++ roy_floyd.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int n, a[10][10], dp[10], dm[10], c[10][10];
+int inf=50000;
+
+void citeste()
+{
+    fstream f("graf.in");
+    int i,x,y;
+    f>>n;
+    while(f >> x >> y) a[x][y] = 1;
+    f.close();
+}
+void afisare(int a[10][10])
+{
+    cout << "\nMatricea:\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+    cout<<endl;
+}
+void vect_grade()
+{
+    int i,j;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            if(a[j][i]==1)dm[i]++;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            if(a[i][j]==1)dp[i]++;
+}
+void afis_vect(int v[10])
+{
+    int i;
+    cout<<"Vectorul: "<<endl;
+    for(i=1;i<=n;i++)
+        cout<<v[i]<<" ";
+    cout<<endl;
+}
+void matr_cost()
+{
+    fstream f("cost.txt");
+    int i,j,x,y,z;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            if(i==j)c[i][j]=0;
+            else c[i][j]=inf;
+    while(f>>i>>j>>z)
+        c[i][j]=z;
+    f.close();
+}
+
+void roy_floyd()
+{
+
+    for(int k = 1 ; k <= n ; k ++)
+        for(int i = 1 ; i <= n ; i ++)
+            for(int j = 1 ; j <= n ; j ++)
+            if(c[i][j] > c[i][k] + c[k][j])
+                c[i][j] = c[i][k] + c[k][j];
+}
+
+int main()
+{
+    int x,y;
+    citeste();
+    afisare(a);
+    vect_grade();
+    afis_vect(dm);
+    afis_vect(dp);
+    matr_cost();
+    afisare(c);
+    roy_floyd();
+    afisare(c);
+    cout<<"De la ce nod pleci?";cin>>x;
+    cout<<"La ce nod ajungi?";cin>>y;
+    if(c[x][y]==inf)cout<<"Nu exista drum."<<endl;
+    else cout<<"Costul drumului "<<c[x][y]<<". "<<endl;
+
+}
